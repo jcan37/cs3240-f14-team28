05f0d104f28aa76a1b517e502213598036e33ee4
Nicholas Bergh <nicholasabergh@gmail.com>
2014-11-10 21:15:22 -0500

    added postbulleting view placeholder

---
+++ project/securewitness/urls.py
@@ -0,0 +1,4 @@
+from django.conf.urls import patterns, url
+from securewitness import views
+
+urlpatterns = patterns('')
--- project/securewitness/views.py
+++ project/securewitness/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def postbulletin(request):
+	return HttpResponse('This is where you will post a bulletin.')
