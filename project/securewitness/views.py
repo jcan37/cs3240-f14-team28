79db0553de44667869288477337c4983cf9be212
Nicholas Bergh <nicholasabergh@gmail.com>
2014-11-13 21:44:02 -0500

    basic bulletin post page added

--- project/securewitness/views.py
+++ project/securewitness/views.py
@@ -1,5 +1,35 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext, loader
+from django import forms
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.utils import timezone
+from models import Bulletin, File
+
+
+
+class BulletinForm(forms.Form):
+	description = forms.CharField(label='Description', max_length=1000)
+	location = forms.CharField(label='Location', max_length=200)
+	files = forms.FileField(label='Files')
 
 def postbulletin(request):
-	return HttpResponse('This is where you will post a bulletin.')
+	if request.method == 'POST':
+		form = BulletinForm(request.POST, request.FILES)
+		if form.is_valid():
+			newBulletin = Bulletin(author=request.user, pub_date=timezone.now(), description=form.cleaned_data['description'], location=form.cleaned_data['location'])
+			newBulletin.save()
+
+			bulletinFile = File(bulletin=newBulletin)
+			bulletinFile.save()
+
+			f = request.FILES['files']
+			with open('securewitness/files/' + str(bulletinFile.id) + '_' + request.FILES['files'].name, 'wb') as destination:
+				for chunk in f.chunks():
+					destination.write(chunk)
+
+			return render(request, 'securewitness/bulletinposted.html')
+	else:
+		form = BulletinForm()
+	
+	return render(request, 'securewitness/postbulletin.html', {'form': form})
+
