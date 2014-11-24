1393c96ba5916e36a27af3a0237d8e03e19ff119
Shane Matthews <shmatthews94@gmail.com>
2014-11-23 21:43:00 -0500

    changed search options

--- project/securewitness/search.py
+++ project/securewitness/search.py
@@ -1,7 +1,13 @@
 from models import Bulletin
+from users import retrieve_user_state
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-def search(field):
-    	results = Bulletin.objects.all().filter(field in description)
-        print results
+def search(request):
+    	context = retrieve_user_state(request)
+    	bulletin_list = Bulletin.objects.all()
+    	results = []
+    	if bulletin_list is not None and len(bulletin_list) > 0:
+    		results = bulletin_list.filter(field in Bulletin.description)
         context['results'] = results
     	return render(request, 'searchresults.html', context)
