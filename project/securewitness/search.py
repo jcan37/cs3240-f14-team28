dbaa3e6ed67354a39889e58a3507b50ef1d3e122
shmatthews94 <som4sf@virginia.edu>
2014-12-03 18:51:38 -0500

    Update search.py

--- project/securewitness/search.py
+++ project/securewitness/search.py
@@ -16,6 +16,6 @@ def search(field, user):
     if bulletin_list is not None and len(bulletin_list) > 0:
     	for entry in bulletin_list:
             if entry not in results:
-                if field.lower() in name.get_username().lower() or field.lower() in entry.location.lower() or field.lower() in entry.description.lower():
+                if field.lower() in user.get_username().lower() or field.lower() in entry.location.lower() or field.lower() in entry.description.lower():
                     results.append(entry)
     return results
